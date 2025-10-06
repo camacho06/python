b77b5afa1d51ecb7c2bcfff4b500bd993f47c78b
Cristhian Camacho Tinoco <cristhiancamachotinoco@gmail.com>
2025-10-06 12:41:32 +0200

    ejercicio1 tipos de datos simples

---
+++ ejercicio7.py
@@ -0,0 +1,4 @@
+peso=input("introduce tu peso ")
+altura=input("introduce tu altura ")
+imc=float(peso)/(float(altura)**2)
+print("tu imc es ") , print(imc)
---
+++ ejercicio9.py
@@ -0,0 +1,8 @@
+capital=input("introduce el capital ")
+tasainteres=capital*0.10
+tiempo=input("introduce el tiempo ")
+c=float(capital)*float(tiempo)
+print("el interes es ") , print(tasainteres)
+print("el total es ") , print(c+tasainteres)
+
+
