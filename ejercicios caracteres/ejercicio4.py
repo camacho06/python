3396edeecf000f0d4d6b69000e57acb433f39b9a
Cristhian Camacho Tinoco <cristhiancamachotinoco@gmail.com>
2025-10-23 10:39:19 +0200

    ejercicio4 y 5 caracteres

---
+++ ejercicios caracteres/ejercicio4.py
@@ -0,0 +1,3 @@
+numerodetelefonos=input("Ingrese su numero de telefono: ")
+x=numerodetelefonos.split("-")
+print("El numero de telefono es : " + x[1])
---
+++ ejercicios caracteres/ejercicio5.py
@@ -0,0 +1,2 @@
+frase=input("Ingrese una frase: ")
+print("La frase invertida es : " + str(frase)[::-1])
