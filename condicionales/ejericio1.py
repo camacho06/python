9a7cc71ed210ac81a51dee78087064f91e5241ac
Cristhian Camacho Tinoco <cristhiancamachotinoco@gmail.com>
2025-10-30 10:29:01 +0100

    ejercicio1 condi

---
+++ condicionales/ejericio1.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+if edad >= 18:
+    print(f"{nombre} es mayor de edad.")
+else:
+    print(f"{nombre} es menor de edad.")
