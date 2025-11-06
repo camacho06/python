15a1ee00acc8a7b2c27aed6cae73238806ed3be8
Cristhian Camacho Tinoco <cristhiancamachotinoco@gmail.com>
2025-11-06 10:58:20 +0100

    ejercicio10 condi modificado

--- condicionales/ejercicio10.py
+++ condicionales/ejercicio10.py
@@ -1,10 +1,10 @@
-pizza = input("Quieres una pizza vegetariana o una normal? ").lower
+pizza = input("Quieres una pizza vegetariana o una normal? ")
 if pizza == "vegetariana":
     print("Los ingredientes de una pizza de perrosflautas vegetarianos son:" \
     " Pimiento y tofu")
-    ingrediente = input("Introduce tu ingrediente a elegir")
+    ingrediente = input("Introduce tu ingrediente a elegir: ")
 else:
     print("Los ingredientes de una pizza normal son: " \
     " Peperoni, Jamón y Salmón")
-    ingrediente = input("Introduce tu ingrediente a elegir")
-print("Su pizza es " ) + pizza + " y lleva " + ingrediente
+    ingrediente = input("Introduce tu ingrediente a elegir: ")
+print("Su pizza es "+ pizza + " y lleva mozzarella , tomate y " + ingrediente)
