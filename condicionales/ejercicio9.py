bab9abcabc2a1e045d42ef28cf9dc6f1b29a9891
Cristhian Camacho Tinoco <cristhiancamachotinoco@gmail.com>
2025-11-06 10:40:37 +0100

    ejercicio9 condi

---
+++ condicionales/ejercicio9.py
@@ -0,0 +1,7 @@
+edad=int(input("Cuantos anos tienes?: "))
+if edad < 4:
+    print("Tu entrada es gratuita")
+elif edad > 4 and edad < 18:
+    print("Tu entrada tiene un coste de 5€ ")
+else:
+    print("Tu entrada tiene un coste de 10€ ")
