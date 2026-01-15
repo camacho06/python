34efdd1ad540e43997e158a641f30a599b9989db
Cristhian Camacho Tinoco <cristhiancamachotinoco@gmail.com>
2026-01-15 10:43:07 +0100

    ejercicio6 diccionarios

---
+++ diccionarios/ejercicio6.py
@@ -0,0 +1,15 @@
+datos = {'nombre': '', 'edad': '', 'sexo': '', 'telefono': '' , 'email': ''}
+nombre = input("Introduce tu nombre: ")
+datos['nombre'] = nombre
+print (datos)
+edad = input("Introduce tu edad: ")
+datos['edad'] = edad
+print (datos)
+sexo = input("Introduce tu sexo: ")
+datos['sexo'] = sexo
+print (datos)
+telefono = input("Introduce tu telefono: ")
+datos['telefono'] = telefono
+print (datos)
+datos['email'] = input("Introduce tu email: ")
+print (datos)
